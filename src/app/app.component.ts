8c0e46d0b4453a952faf7a795794d61163de37e5
Emad Khan <emadkhanqai@gmail.com>
2022-06-25 02:23:27 +0500

    Change box style dynamically through tem ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,20 @@ export class AppComponent {
   getValue(value: any) {
     this.title = value;
   }
+  changeColor(element: any) {
+    element.style.background = 'red';
+  }
+  addClass(element: any) {
+    element.classList.add('boom');
+  }
 
+  validateAge(age: any) {
+    if (parseInt(age.value) > 18) {
+      age.classList.remove('green');
+      age.classList.add('red');
+    } else {
+      age.classList.remove('red');
+      age.classList.add('green');
+    }
+  }
 }
