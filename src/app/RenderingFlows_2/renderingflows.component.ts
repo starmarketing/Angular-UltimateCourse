4a9b356739e9b207e4c5649dc8144a18621b5b27
Emad Khan <emadkhanqai@gmail.com>
2022-06-25 03:55:22 +0500

    Discussed aboiut ngif and ngfor

--- src/app/RenderingFlows_2/renderingflows.component.ts
+++ src/app/RenderingFlows_2/renderingflows.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Employee{
+  id: number;
+  name: string;
+  active: boolean;
+}
+
 @Component({
   selector: 'rendering-flows',
   templateUrl: './renderingflows.component.html',
-  styleUrls: ['./renderingflows.component.scss']
+  styleUrls: ['./renderingflows.component.scss'],
 })
 export class RenderingflowsComponent implements OnInit {
+  age: string = '';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleChange(param: any) {
+    this.age = param.target.value;
   }
 
+  employees: Employee[] = [
+    {
+      id: 1,
+      name: 'emad',
+      active: true,
+    },
+    {
+      id: 2,
+      name: 'khan',
+      active: true,
+    },
+  ];
 }
