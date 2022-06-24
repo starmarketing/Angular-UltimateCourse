3e627674ea311b89ea1c3b46227009fca97bb1ff
Emad Khan <emadkhanqai@gmail.com>
2022-06-25 02:11:31 +0500

    Property binding and Ref Variable Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'practice';
+  total: any = '';
+
+  changeText() {
+    this.title = 'hahhaha';
+  }
+  changeTextWithNgModelChange(value: any) {
+    this.title = value;
+  }
+  generateTotal(event: any) {
+    if (!isNaN(event.target.value)) {
+      this.total = parseInt(event.target.value) * 2;
+    }
+  }
+
+  getValue(value: any) {
+    this.title = value;
+  }
+
 }
