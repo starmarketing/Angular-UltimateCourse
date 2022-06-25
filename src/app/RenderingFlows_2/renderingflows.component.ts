3964b0abac45ccafc9816abda4a62b39f8a64e45
Emad Khan <emadkhanqai@gmail.com>
2022-06-25 14:34:12 +0500

    Safe Navigation Operator Discussed

--- src/app/RenderingFlows_2/renderingflows.component.ts
+++ src/app/RenderingFlows_2/renderingflows.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Child {
+  name: string;
+  age: number | null;
+}
 interface Employee{
   id: number;
   name: string;
   active: boolean;
   stamp: number | null;
+  children: Child[] | null
 }
 
 @Component({
@@ -27,13 +32,28 @@ export class RenderingflowsComponent implements OnInit {
       id: 1,
       name: 'emad',
       active: true,
-      stamp: 1508330494000
+      stamp: 1508330494000,
+      children: [{
+        name: 'Badshah',
+        age: 20
+      }]
+    },
+    {
+      id: 2,
+      name: 'hassan',
+      active: false,
+      stamp: null,
+      children: null
     },
     {
       id: 2,
       name: 'khan',
       active: false,
-      stamp: null
+      stamp: null,
+      children: [{
+        name: 'Khan Sahab',
+        age: null
+      }]
     },
   ];
 }
