50eab11975bfab3d1058717b9e0618fafb425071
Emad Khan <emadkhanqai@gmail.com>
2022-06-29 02:53:39 +0500

    Routing introduced with HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+import { HomeComponent } from './home-component/home-component.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, PassengerDashboardModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    PassengerDashboardModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
