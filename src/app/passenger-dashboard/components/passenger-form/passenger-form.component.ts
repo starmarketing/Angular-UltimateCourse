b430f625dc97e85e766e80640f7010203a9a9cb3
Emad Khan <emadkhanqai@gmail.com>
2022-06-29 02:08:34 +0500

    select option rendering, and ngValue

--- src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,24 +1,42 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Baggage } from '../../models/baggage.interface';
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-form',
   templateUrl: './passenger-form.component.html',
-  styleUrls: ['./passenger-form.component.scss']
+  styleUrls: ['./passenger-form.component.scss'],
 })
 export class PassengerFormComponent implements OnInit {
-
   @Input()
   detail: Passenger | undefined;
   // detail?: Passenger = {} as Passenger;
 
-  constructor() { }
+  baggage: Baggage[] = [
+    {
+      key: 'none',
+      value: 'No baggage',
+    },
+    {
+      key: 'hand-only',
+      value: 'Hand baggage',
+    },
+    {
+      key: 'hold-only',
+      value: 'Hold baggage',
+    },
+    {
+      key: 'hold-hand',
+      value: 'Hand & Hold baggage',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
   toggleCheckIn(checkedIn: boolean) {
-    if(checkedIn) {
+    if (checkedIn) {
       this.detail!.checkInDate = Date.now();
     }
   }
